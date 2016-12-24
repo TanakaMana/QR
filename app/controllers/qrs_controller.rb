77dd3b0f4049825886c6b1722564280408cd3186
田中 真奈 <makkuromyaon@gmail.com>
2016-12-25 04:25:43 +0900

    add action at qrs_controller

--- app/controllers/qrs_controller.rb
+++ app/controllers/qrs_controller.rb
@@ -1,2 +1,14 @@
 class QrsController < ApplicationController
+  before_action :set_url, only: :create
+  der index
+  end
+
+  def create
+    @qr = QrcodeServise.new.create_qr(@url)
+  end
+
+  private
+  def set_url
+    @url = params[:url]
+  end
 end
