9d982f80733c04e827f4bad2e31f8797bcd28291
田中 真奈 <makkuromyaon@gmail.com>
2016-12-25 14:06:07 +0900

    delete unnecessary gem and file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
-  layout 'admin_lte_2'
-
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
 end
